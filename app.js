20c868615cf2096e01e782c3505bce3fcb05b2f5
abdull1211 <68056683+abdull1211@users.noreply.github.com>
2023-06-10 12:52:12 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const currentTime = new Date().toLocaleTimeString();
+  let greeting = '';
+
+  if (currentTime < '12:00') {
+    greeting = 'Good morning!';
+  } else if (currentTime < '18:00') {
+    greeting = 'Good afternoon!';
+  } else {
+    greeting = 'Good evening!';
+  }
+
+  res.send(`${greeting} Waktu saat ini adalah ${currentTime} ~xdule`);
+});
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
